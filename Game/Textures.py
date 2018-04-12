0901742ff1c3510ae358046bb669580a7907a0df
Camille Girard <camille.girard@students.olin.edu>
2018-04-12 00:02:50 -0400

    Updated game graphics

---
+++ Game/Textures.py
@@ -0,0 +1,33 @@
+import pygame
+
+pygame.init()
+
+class Tiles:
+
+    size = 32
+
+    blocked = []
+
+    blocked_types = ["3"]
+
+    def blocked_at(pos):
+        if list(pos) in Tiles.blocked:
+            return True
+        else:
+            return False
+
+    def load_texture(file, size):
+        bitmap = pygame.image.load(file)
+        bitmap = pygame.transform.scale(bitmap, (size, size))
+
+        surface = pygame.Surface((size, size), pygame.HWSURFACE|pygame.SRCALPHA)
+        surface.blit(bitmap, (0,0))
+
+        return surface
+
+    grass = load_texture("Graphics/grass.png", size)
+    stone = load_texture("Graphics/stone.png", size)
+    water = load_texture("Graphics/water.png", size)
+    dirt = load_texture("Graphics/dirt.png", size)
+
+    texture_tags = {"1": grass, "2":stone, "3": water, "4": dirt}
---
+++ Game/player.py
@@ -0,0 +1,21 @@
+import pygame
+from NPC import *
+
+pygame.init()
+
+class Player:
+
+    def __init__(self,name):
+        self.name = name
+        self.facing = "south"
+        self.health = 100
+        sprite = pygame.image.load("Graphics/player.png")
+        size = sprite.get_size()
+        self.width = size[0]
+        self.height = size[1]
+
+        # Get player faces
+        self.faces = get_faces(sprite)
+
+    def render(self, surface, pos):
+        surface.blit(self.faces[self.facing], pos)
