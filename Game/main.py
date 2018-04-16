726cd96d67f782cbc54e0dba4116d06c1d2d98bc
scalvo24 <sebastian.calvo@students.olin.edu>
2018-04-16 16:40:16 -0400

    main menu added

--- Game/main.py
+++ Game/main.py
@@ -5,6 +5,8 @@ from mapengine import *
 from player import *
 from NPC import *
 from inventory import *
+from meloonatic_gui import *
+from UltraColor import *
 
 pygame.init()
 
@@ -67,6 +69,33 @@ player_x = round(window_width/2 - player_w/2 - glob.Globals.camera_x) / Tiles.si
 player_y = round(window_height/2 - player_h/2 - glob.Globals.camera_y) / Tiles.size
 
 item = 'none'
+
+#INITIALIZE GUI
+def Play():
+    glob.Globals.scene = 'game'
+
+def Exit():
+    global isRunning
+    isRunning = False
+
+btnPlay = Menu.Button(text = "Play", rect = (0,0,500,60),
+                                bg = Color.Gray, fg = Color.White,
+                                bgr = Color.CornflowerBlue, tag = ("menu", None))
+btnPlay.Left = window_width /2 - btnPlay.Width/2
+btnPlay.Top = window_height/2 - btnPlay.Height/2
+btnPlay.Command = Play
+
+btnExit = Menu.Button(text = 'Exit', rect = (0,0,500,60),
+                        bg = Color.Gray, fg = Color.White,
+                        bgr = Color.CornflowerBlue, tag = ("menu", None))
+btnExit.Left = btnPlay.Left
+btnExit.Top = btnPlay.Top + btnExit.Height + 3
+btnExit.Command = Exit
+
+menuTitle = Menu.Text(text = 'GLASBOT', color = Color.Cyan, font = Font.Large)
+
+menuTitle.Left, menuTitle.Top = window_width/2 - menuTitle.Width/2, 0
+
 isRunning = True
 #Variable that is consistent when the game is running
 
@@ -111,47 +140,68 @@ while isRunning:
         elif event.type == pygame.KEYUP:
             glob.Globals.camera_move = 0
 
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: #LEFT CLICK
+                for btn in Menu.Button.All:
+                    if btn.Tag[0] == glob.Globals.scene and btn.Rolling:
+                        if btn.Command != None:
+                            btn.Command() #DO BUTTON EVENT
+                        btn.Rolling = False
+                        break
+
+    #RENDER SCENEelif glob.Globals.scene == 'menu':
+
+
+    if glob.Globals.scene == 'game':
+
     #LOGIC
 
-    if glob.Globals.camera_move == 1:
-        if not Tiles.blocked_at((round(player_x),math.floor(player_y))):
-            glob.Globals.camera_y += deltatime * 100
-    elif glob.Globals.camera_move == 2:
-        if not Tiles.blocked_at((round(player_x),math.ceil(player_y))):
-            glob.Globals.camera_y -= deltatime * 100
-    elif glob.Globals.camera_move == 3:
-        if not Tiles.blocked_at((math.floor(player_x),round(player_y))):
-            glob.Globals.camera_x += deltatime * 100
-    elif glob.Globals.camera_move == 4:
-        if not Tiles.blocked_at((math.ceil(player_x),round(player_y))):
-            glob.Globals.camera_x -= deltatime * 100
+        if glob.Globals.camera_move == 1:
+            if not Tiles.blocked_at((round(player_x),math.floor(player_y))):
+                glob.Globals.camera_y += deltatime * 100
+        elif glob.Globals.camera_move == 2:
+            if not Tiles.blocked_at((round(player_x),math.ceil(player_y))):
+                glob.Globals.camera_y -= deltatime * 100
+        elif glob.Globals.camera_move == 3:
+            if not Tiles.blocked_at((math.floor(player_x),round(player_y))):
+                glob.Globals.camera_x += deltatime * 100
+        elif glob.Globals.camera_move == 4:
+            if not Tiles.blocked_at((math.ceil(player_x),round(player_y))):
+                glob.Globals.camera_x -= deltatime * 100
 
-    player_x = (window_width/2 - player_w/2 - glob.Globals.camera_x) / Tiles.size
-    player_y = (window_height/2 - player_h/2 - glob.Globals.camera_y) / Tiles.size
+        player_x = (window_width/2 - player_w/2 - glob.Globals.camera_x) / Tiles.size
+        player_y = (window_height/2 - player_h/2 - glob.Globals.camera_y) / Tiles.size
 
-    #RENDER GRAPHICS
-    #Makes the background filled black
-    # window.fill((0,0,0))
+        #RENDER GRAPHICS
+        #Makes the background filled black
+        # window.fill((0,0,0))
 
-    window.blit(Sky, (0,0))
+        window.blit(Sky, (0,0))
 
-    window.blit(terrain,(glob.Globals.camera_x, glob.Globals.camera_y))
+        window.blit(terrain,(glob.Globals.camera_x, glob.Globals.camera_y))
 
-    player.render(window, (window_width/2 - player_w/2, window_height/2 - player_h/2))
+        player.render(window, (window_width/2 - player_w/2, window_height/2 - player_h/2))
 
-    text = fps_font.render("Score: %s" % player.points, 1, (255, 0, 0))
-    textpos = text.get_rect(centerx= 200, centery = 50)
-    window.blit(text, textpos)
+        text = fps_font.render("Score: %s" % player.points, 1, (255, 0, 0))
+        textpos = text.get_rect(centerx= 200, centery = 50)
+        window.blit(text, textpos)
 
-    htext = fps_font.render("Health: %s" % player.health, 1, (255, 0, 0))
-    hpos = htext.get_rect(centerx = 100, centery = 50)
-    window.blit(htext, hpos)
+        htext = fps_font.render("Health: %s" % player.health, 1, (255, 0, 0))
+        hpos = htext.get_rect(centerx = 100, centery = 50)
+        window.blit(htext, hpos)
 
+        #PROCESS MENU
+    elif glob.Globals.scene == 'menu':
+        window.fill(Color.Fog)
+
+        menuTitle.Render(window)
+
+        for btn in Menu.Button.All:
+            if btn.Tag[0] == "menu":
+                btn.Render(window)
 
     show_fps()
-
     pygame.display.update()
-
     count_fps()
 
 pygame.quit()
